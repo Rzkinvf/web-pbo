71eb55e1997006e71bd88ea023b8e000b188bb35
Rzkinvf <111211165+Rzkinvf@users.noreply.github.com>
2022-12-25 05:46:08 -0800

    Add files via upload

---
+++ routes/employeeRoute.js
@@ -0,0 +1,8 @@
+import { getEmployee } from "../controllers/employeeController.js";
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/profile', getEmployee)
+
+export default router;
